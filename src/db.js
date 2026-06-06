2c140468dd5753ecb28112c62057837a6bbd4aaa
Asmita <asmitadey2602@gmail.com>
2026-06-06 19:39:13 +0000

    add dexie cloud sync

--- src/db.js
+++ src/db.js
@@ -1,17 +1,24 @@
 import Dexie from 'dexie'
+import dexieCloud from 'dexie-cloud-addon'
 
-export const db = new Dexie('KitnaHua')
+export const db = new Dexie('KitnaHua', { addons: [dexieCloud] })
 
 db.version(2).stores({
-  days: 'date, dayType',
-  tasks: '++id, date, title, tag, priority, points, completed, feedbackDone, dayTypeTemplate',
-  feedback: 'date',
-  rewards: '++id, name, cost',
-  redemptions: '++id, date, rewardId, name, cost',
-  aiAnalysis: 'date, score, rating, analysis, recommendation, reclassifiedType',
-  subjects: '++id, name, description',
-  topics: '++id, subjectId, name, totalLectures',
-  lectures: '++id, topicId, subjectId, name, watched, notesMade, questionsSolved, revisionDone, lastStudied',
+  days: '@date, dayType',
+  tasks: '@id, date, title, tag, priority, points, completed, feedbackDone, dayTypeTemplate',
+  feedback: '@date',
+  rewards: '@id, name, cost',
+  redemptions: '@id, date, rewardId, name, cost',
+  aiAnalysis: '@date, score, rating, analysis, recommendation, reclassifiedType',
+  subjects: '@id, name, description',
+  topics: '@id, subjectId, name, totalLectures',
+  lectures: '@id, topicId, subjectId, name, watched, notesMade, questionsSolved, revisionDone, lastStudied',
+})
+
+db.cloud.configure({
+  databaseUrl: 'https://ztzyryvif.dexie.cloud',
+  requireAuth: false,
+  tryUseServiceWorker: false,
 })
 
 db.on('populate', () => {
